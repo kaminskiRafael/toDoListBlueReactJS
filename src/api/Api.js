a1522722c3df5bb720b7833985bd81ad780245b9
kaminskiRafael <rafael@rfkdev.com.br>
2021-10-19 22:40:33 -0300

    conserto do API>JS

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,5 +1,5 @@
 const Api = {
-    apiUrl: 'http://localhost:4000/todolist',
+    apiUrl: 'https://rfktodolistnode.herokuapp.com/todolist',
     fetchGetAll: () => fetch(Api.apiUrl),
     fetchGetById: id => fetch(`${Api.apiUrl}/${id}`),
     fecthPost: (tarefa) => {
